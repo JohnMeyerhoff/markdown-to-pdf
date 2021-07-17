24fe960fba7e719b43b32a0b2f37e8a679c84411
Neo Hornberger <neo.hornberger@gmx.de>
2021-07-17 23:34:48 +0200

    Added missing else that will crash the process

--- src/docker_entry.js
+++ src/docker_entry.js
@@ -8,9 +8,9 @@ if(process.env['CI']) {
 	// if $GITHUB_ACTIONS is set, this container is run by GitHub Actions
 	if(process.env['GITHUB_ACTIONS']) {
 		INTERFACE('INPUT_', '/github/workspace/');
+	}else {
+		throw 'Environment variable $CI is set, but runner could not be identified!';
 	}
-	
-	throw 'Environment variable $CI is set, but runner could not be identified!';
 }else {
 	INTERFACE();
 }
